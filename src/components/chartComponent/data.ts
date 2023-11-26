a7e9cdbd64ccc02ac0d55b7e64c458a314f1e70d
Shozab Khan <khan.shozab.93@gmail.com>
2023-11-26 14:20:40 -0500

    test results data created

---
+++ src/components/chartComponent/data.ts
@@ -0,0 +1,198 @@
+export const testResults = [
+  {
+    id: '0',
+    status: 'high',
+    name: 'Total Cholesterol',
+    value: '6 mmol/L',
+    chartData: [
+      {
+        monthYear: "09/21'",
+        uv: 1.5,
+      },
+      {
+        monthYear: "10/21'",
+        uv: 2,
+      },
+      {
+        monthYear: "11/21'",
+        uv: 2.6,
+      },
+      {
+        monthYear: "12/21'",
+        uv: 2.6,
+      },
+      {
+        monthYear: "01/22'",
+        uv: 2,
+      },
+    ],
+  },
+  {
+    id: '1',
+    status: 'optimal',
+    name: 'AST',
+    value: '2 mmol/L',
+    chartData: [
+      {
+        monthYear: "09/21'",
+        uv: 1.5,
+      },
+      {
+        monthYear: "10/21'",
+        uv: 2,
+      },
+      {
+        monthYear: "11/21'",
+        uv: 2.6,
+      },
+      {
+        monthYear: "12/21'",
+        uv: 2.6,
+      },
+      {
+        monthYear: "01/22'",
+        uv: 2,
+      },
+    ],
+  },
+  {
+    id: '2',
+    status: 'optimal',
+    name: 'GGT',
+    value: '2 mmol/L',
+    chartData: [
+      {
+        monthYear: "09/21'",
+        uv: 1.5,
+      },
+      {
+        monthYear: "10/21'",
+        uv: 2,
+      },
+      {
+        monthYear: "11/21'",
+        uv: 2.6,
+      },
+      {
+        monthYear: "12/21'",
+        uv: 2.6,
+      },
+      {
+        monthYear: "01/22'",
+        uv: 2,
+      },
+    ],
+  },
+  {
+    id: '3',
+    status: 'optimal',
+    name: 'ALP',
+    value: '2 mmol/L',
+    chartData: [
+      {
+        monthYear: "09/21'",
+        uv: 1.5,
+      },
+      {
+        monthYear: "10/21'",
+        uv: 2,
+      },
+      {
+        monthYear: "11/21'",
+        uv: 2.6,
+      },
+      {
+        monthYear: "12/21'",
+        uv: 2.6,
+      },
+      {
+        monthYear: "01/22'",
+        uv: 2,
+      },
+    ],
+  },
+  {
+    id: '4',
+    status: 'optimal',
+    name: 'Total Protein',
+    value: '2 mmol/L',
+    chartData: [
+      {
+        monthYear: "09/21'",
+        uv: 1.5,
+      },
+      {
+        monthYear: "10/21'",
+        uv: 2,
+      },
+      {
+        monthYear: "11/21'",
+        uv: 2.6,
+      },
+      {
+        monthYear: "12/21'",
+        uv: 2.6,
+      },
+      {
+        monthYear: "01/22'",
+        uv: 2,
+      },
+    ],
+  },
+  {
+    id: '5',
+    status: 'optimal',
+    name: 'Albumin',
+    value: '2 mmol/L',
+    chartData: [
+      {
+        monthYear: "09/21'",
+        uv: 1.5,
+      },
+      {
+        monthYear: "10/21'",
+        uv: 2,
+      },
+      {
+        monthYear: "11/21'",
+        uv: 2.6,
+      },
+      {
+        monthYear: "12/21'",
+        uv: 2.6,
+      },
+      {
+        monthYear: "01/22'",
+        uv: 2,
+      },
+    ],
+  },
+  {
+    id: '6',
+    status: 'optimal',
+    name: 'Globulin',
+    value: '2 mmol/L',
+    chartData: [
+      {
+        monthYear: "09/21'",
+        uv: 1.5,
+      },
+      {
+        monthYear: "10/21'",
+        uv: 2,
+      },
+      {
+        monthYear: "11/21'",
+        uv: 2.6,
+      },
+      {
+        monthYear: "12/21'",
+        uv: 2.6,
+      },
+      {
+        monthYear: "01/22'",
+        uv: 2,
+      },
+    ],
+  },
+];
