70bd6726fe4c0662690195cae4dcfd922a634121
Shozab Khan <khan.shozab.93@gmail.com>
2023-11-26 14:19:53 -0500

    DotIcon props added

--- src/components/chartComponent/components/svgs.tsx
+++ src/components/chartComponent/components/svgs.tsx
@@ -1,6 +1,6 @@
-export const DotIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="6" height="7" viewBox="0 0 6 7" fill="none">
-    <circle cx="3" cy="3.69092" r="3" fill="#77A06C" />
+export const DotIcon = ({ color, size }: { color: string; size: number }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 6 7" fill="none">
+    <circle cx="3" cy="3.69092" r="3" fill={color} />
   </svg>
 );
 export const SeeMoreIcon = () => (
