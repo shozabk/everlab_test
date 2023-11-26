6788fdfa3a8e5331a3ab96b74d73fa90c5e546ca
Shozab Khan <khan.shozab.93@gmail.com>
2023-11-26 14:19:36 -0500

    data handled with props

--- src/components/chartComponent/ChartComponent.tsx
+++ src/components/chartComponent/ChartComponent.tsx
@@ -1,84 +1,81 @@
 /* eslint-disable react/jsx-closing-tag-location */
 /* eslint-disable react/self-closing-comp */
-import './chart-component.css';
-
+import { FC } from 'react';
 import { LineChart, CartesianGrid, XAxis, YAxis, Line } from 'recharts';
+
 import { DotIcon, SeeMoreIcon } from './components/svgs';
+import chart from './ChartComponent.module.css';
+import { TestResult } from './chart-component.type';
 
-const ChartComponent = () => {
-  const data = [
-    {
-      monthYear: "09/21'",
-      uv: 1.5,
-    },
-    {
-      monthYear: "10/21'",
-      uv: 2,
-    },
-    {
-      monthYear: "11/21'",
-      uv: 2.6,
-    },
-    {
-      monthYear: "12/21'",
-      uv: 2.6,
-    },
-    {
-      monthYear: "01/22'",
-      uv: 2,
-    },
-  ];
+interface ChartComponentI {
+  result: TestResult;
+}
+
+const greenColor = '#77A06C';
+const redColor = '#FF6B6B';
+
+const ChartComponent: FC<ChartComponentI> = ({ result }) => {
+  const { value, chartData, status, name } = result;
+
   const yTicks = [1.0, 2.6];
+  const CustomActiveDot: FC<any> = (props: any) => {
+    const { cx, cy, stroke, payload, index } = props;
+    const { uv, monthYear } = payload;
+    const isLastPoint = index === chartData.length - 1;
+    const selectedColor = '#221F20';
 
-  const CustomTick = ({ x, y, payload }) => {
-    if (payload.index === data.length - 1) {
+    if (isLastPoint) {
       return (
-        <g transform={`translate(${x},${y})`}>
-          <text x={0} y={0} dy={16} textAnchor="end" fill="#666">
-            {payload.value}
-          </text>
+        <g>
+          <circle cx={cx} cy={cy} r={6} stroke={selectedColor} fill="#fff" strokeWidth={2} />
+          <line
+            x1={cx}
+            y1={cy}
+            x2={cx}
+            y2={cy + uv * 5 + 50}
+            stroke={selectedColor}
+            strokeWidth={2}
+            strokeDasharray="3 3"
+          />
+          <text
+            x={cx}
+            y={cy + uv * 5 + 60}
+            textAnchor="middle"
+            fill={selectedColor}
+          >{`${monthYear}`}</text>
         </g>
       );
     }
-    return null;
-  };
 
-  const CustomizedDot = (props) => {
-    const { cx, cy, stroke, payload } = props;
-
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="20"
-        height="21"
-        viewBox="0 0 20 21"
-        fill="none"
-      >
-        <circle cx="20" cy="20" r="20" stroke="black" strokeWidth="3" fill="red" />
-      </svg>
-    );
+    return <circle cx={cx} cy={cy} r={4} stroke={stroke} fill="white" strokeWidth={2} />;
   };
 
   return (
-    <div className="card-layout">
-      <div className="card-inner-layout">
-        <div className="first-row">
+    <div className={chart.cardLayout}>
+      <div className={chart.cardInnerLayout}>
+        <div className={chart.firstRow}>
           <span>
-            <DotIcon />
-            <span className="heading">OPTIMAL</span>
+            <DotIcon size={8} color={status === 'optimal' ? greenColor : redColor} />
+            <span className={chart.heading}>{status}</span>
           </span>
           <SeeMoreIcon />
         </div>
-        <div className="second-row">
-          <span className="heading2 color-black">ALP</span>
-          <DotIcon />
-          <span className="heading2 color-green">2 mmol/L</span>
+        <div className={chart.secondRow}>
+          <span className={`${chart.heading2} ${chart.colorBlack}`}>{name}</span>
+          <DotIcon size={5} color="black" />
+          <span
+            className={`${chart.heading2} ${
+              status === 'optimal' ? chart.colorGreen : chart.colorRed
+            }`}
+          >
+            {value}
+          </span>
         </div>
-        <div className="chart-div">
+        <div className={chart.chartDiv}>
           <LineChart
             width={320}
             height={100}
-            data={data}
+            data={chartData}
             margin={{
               top: 5,
               right: 30,
@@ -86,29 +83,25 @@ const ChartComponent = () => {
             }}
           >
             <CartesianGrid strokeDasharray="3 3" vertical={false} />
-            <XAxis
-              dataKey="monthYear"
-              axisLine={{ strokeDasharray: '3 3' }}
-              tick={<CustomTick />}
-            />
+            <XAxis dataKey="monthYear" axisLine={{ strokeDasharray: '3 3' }} tick={false} />
             <YAxis
               type="number"
-              domain={[1, 2.6]}
+              domain={yTicks}
               ticks={yTicks}
               axisLine={false}
               tick={{
-                color: '#77A06C',
+                color: greenColor,
                 fontSize: '12px',
                 fontWeight: '600',
               }}
             />
-            <Line type="monotone" dataKey="uv" stroke="#8884D8" dot={<CustomizedDot />} />
+            <Line type="monotone" dataKey="uv" stroke="#AFB4BF" dot={<CustomActiveDot />} />
 
             <defs>
               <linearGradient id="gradient" x1="0" y1="0" x2="0" y2="100%">
-                <stop offset="0%" stopColor="#77A06C" />
-                <stop offset="50%" stopColor="#77A06C" stopOpacity="0.3" />
-                <stop offset="100%" stopColor="#77A06C" stopOpacity="0" />
+                <stop offset="0%" stopColor={greenColor} />
+                <stop offset="50%" stopColor={greenColor} stopOpacity="0.3" />
+                <stop offset="100%" stopColor={greenColor} stopOpacity="0" />
               </linearGradient>
             </defs>
             <rect x={55} y={5} width="75%" height={70} fill="url(#gradient)" fillOpacity={0.3} />
