2505aa7919329bbe9fdbe102609ab4450c0939e1
Shozab Khan <khan.shozab.93@gmail.com>
2023-11-26 14:20:23 -0500

    styles and props to chart component added

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,26 +1,19 @@
 /* eslint-disable import/extensions */
 // import { Line, LineChart } from 'recharts';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { testResults } from '@/components/chartComponent/data';
 import { Welcome } from '../components/Welcome/Welcome';
 import ChartComponent from '@/components/chartComponent/ChartComponent';
-
-// const data = [
-//   { name: 'Page A', uv: 400, pv: 2400, amt: 2400 },
-//   { name: 'Page A', uv: 400, pv: 3243, amt: 234234 },
-// ];
+import styles from './Home.module.css';
 
 export function HomePage() {
-  // const renderLineChart = (
-  //   <LineChart width={400} height={400} data={data}>
-  //     <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-  //   </LineChart>
-  // );
-
   return (
     <>
       <Welcome />
-      <ColorSchemeToggle />
-      <ChartComponent />
+      <div className={styles.resultContainer}>
+        {testResults.map((result) => (
+          <ChartComponent result={result} key={result.id} />
+        ))}
+      </div>
     </>
   );
 }
