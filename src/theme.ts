b1c22911311ee6c363fe4c7b5723c2ff674da74f
Shozab Khan <khan.shozab.93@gmail.com>
2023-11-27 02:41:59 -0500

    font-family added

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,20 @@
 import { createTheme } from '@mantine/core';
 
 export const theme = createTheme({
-  /** Put your mantine theme override here */
+  fontFamily: 'Inter, Verdana, sans-serif',
+
+  colors: {
+    'theme-green': [
+      '#77A06C',
+      '#709D68',
+      '#75A372',
+      '#7AA97D',
+      '#7FAE87',
+      '#85B492',
+      '#8AB99C',
+      '#8FBFA7',
+      '#94C4B1',
+      '#99C9BC',
+    ],
+  },
 });
